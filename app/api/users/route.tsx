da7bedffd22aa19b5a5ca6eded899332a2e21243
jhan25 <36556618+jhan25@users.noreply.github.com>
2024-07-12 15:51:50 +0900

    adding some api routes

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -6,3 +6,16 @@ export function GET(request: NextRequest) {
     { id: 2, name: "mosh.hamedani" },
   ]);
 }
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  if (!body.name) {
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  }
+  return NextResponse.json({ id: 1, name: body.name }, { status: 200 });
+}
+
+export function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {}
